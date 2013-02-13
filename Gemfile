14d65f7914d124422abf6cef18c468e0cd080152
☃ pitr <pitr.vern@gmail.com>
2013-02-13 15:34:03 -0500

    more insignificant cleanup

--- Gemfile
+++ Gemfile
@@ -1,5 +1,4 @@
-source 'https://rubygems.org'
+source :rubygems
 
 # Specify your gem's dependencies in fluent-plugin-elasticsearch.gemspec
 gemspec
-
--- test/plugin/test_out_elasticsearch.rb
+++ test/plugin/test_out_elasticsearch.rb
@@ -1,4 +1,15 @@
-require 'helper'
+require 'test/unit'
+
+require 'fluent/test'
+require 'fluent/plugin/out_elasticsearch'
+
+require 'webmock/test_unit'
+require 'date'
+
+$:.push File.expand_path("../lib", __FILE__)
+$:.push File.dirname(__FILE__)
+
+WebMock.disable_net_connect!
 
 class ElasticsearchOutput < Test::Unit::TestCase
   attr_accessor :index_cmds
