a18803815a983a59fce4dcdc2be32f2a33652e15
☃ pitr <pvernigorov@zynga.com>
2015-12-07 23:34:09 -0500

    fix coveralls (☃ stays in my username)

--- test/helper.rb
+++ test/helper.rb
@@ -8,6 +8,14 @@ end
 require 'coveralls'
 Coveralls.wear!
 
+# needs to be after simplecov but before test/unit, because fluentd sets default
+# encoding to ASCII-8BIT, but coverall might load git data which could contain a
+# UTF-8 character
+at_exit do
+  Encoding.default_internal = 'UTF-8' if defined?(Encoding) && Encoding.respond_to?(:default_internal)
+  Encoding.default_external = 'UTF-8' if defined?(Encoding) && Encoding.respond_to?(:default_external)
+end
+
 require 'test/unit'
 require 'fluent/test'
 require 'minitest/pride'
