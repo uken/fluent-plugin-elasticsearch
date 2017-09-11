040e43733a10a7541008c9581ca06c44099a4422
Hiroshi Hatake <hatake@clear-code.com>
2017-09-11 14:17:42 +0900

    Adding unit test for custom time_key with custom time_key_format
    v0.14 version of #285.

--- test/plugin/test_out_elasticsearch.rb
+++ test/plugin/test_out_elasticsearch.rb
@@ -793,6 +793,21 @@ class ElasticsearchOutput < Test::Unit::TestCase
     assert_equal(index_cmds[1]['@timestamp'], ts)
   end
 
+  def test_uses_custom_time_key_with_format
+    driver.configure("logstash_format true
+                      time_key_format %Y-%m-%d %H:%M:%S.%N%z
+                      time_key vtm\n")
+    stub_elastic_ping
+    stub_elastic
+    ts = "2001-02-03 13:14:01.673+02:00"
+    driver.run(default_tag: 'test') do
+      driver.feed(sample_record.merge!('vtm' => ts))
+    end
+    assert(index_cmds[1].has_key? '@timestamp')
+    assert_equal(index_cmds[1]['@timestamp'], ts)
+    assert_equal("logstash-2001.02.03", index_cmds[0]['index']['_index'])
+  end
+
   def test_uses_custom_time_key_exclude_timekey
     driver.configure("logstash_format true
                       time_key vtm
