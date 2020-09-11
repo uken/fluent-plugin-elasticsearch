b924ff22bc65694c82db81a1cceffed1972554ea
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2020-09-11 11:51:03 +0900

    index_template: Handle error object entirely on index creation failure
    ref: https://github.com/fluent/fluentd/blob/master/lib/fluent/log.rb#L369
    ref: https://github.com/fluent/fluentd/blob/adca0296bfcbb8696afa949a55e82807dd69c155/lib/fluent/log.rb#L511
    
    Signed-off-by: Hiroshi Hatake <cosmo0920.oucc@gmail.com>

--- lib/fluent/plugin/elasticsearch_index_template.rb
+++ lib/fluent/plugin/elasticsearch_index_template.rb
@@ -61,7 +61,7 @@ module Fluent::ElasticsearchIndexTemplate
     if e.message =~ /"already exists"/ || e.message =~ /resource_already_exists_exception/
       log.debug("Index #{index_name} already exists")
     else
-      log.error("Error while index creation - #{index_name}: #{e.inspect}")
+      log.error("Error while index creation - #{index_name}", error: e)
     end
   end
 
