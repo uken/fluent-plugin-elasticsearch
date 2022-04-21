2b407b98fb9ccd4eaa35beddacb6a1c3c26d5fb0
Daniel Schaaff <daniel@danielschaaff.com>
2022-04-21 09:08:41 -0700

    do not overwrite @timestamp in data stream if it already exists

--- lib/fluent/plugin/out_elasticsearch_data_stream.rb
+++ lib/fluent/plugin/out_elasticsearch_data_stream.rb
@@ -248,7 +248,9 @@ module Fluent::Plugin
         end
 
         begin
-          record.merge!({"@timestamp" => Time.at(time).iso8601(@time_precision)})
+          unless record.has_key?("@timestamp")
+            record.merge!({"@timestamp" => Time.at(time).iso8601(@time_precision)})
+          end
           bulk_message = append_record_to_messages(CREATE_OP, {}, headers, record, bulk_message)
         rescue => e
           router.emit_error_event(tag, time, record, e)
